4cfc58710e90f925753862bba004589f4b34bd2e
Jake Runzer <jakerunzer@gmail.com>
2017-03-04 10:49:47 -0800

    dave

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ def start():
         'color': '#00FF00',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
-        'name': 'sneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksnek',
+        'name': 'dave',
         'head_type': 'safe',
         'tail_type': 'freckled'
     }
