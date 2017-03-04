5880666351b3c77b54a52f618c5f03cb52a5a67d
Tyson Battistella <tysonbattistella@gmail.com>
2017-03-04 12:14:20 -0800

    weighted food moves

--- app/utils.py
+++ app/utils.py
@@ -7,3 +7,7 @@ def flatten(l):
 def map_to_one(v, l, h):
     """Map a value v in range (l, h) to (0, 1)."""
     return ((v - l) / (h - l)) * (1 - 0) + 0
+
+def average_length(snakes):
+    """Get average length for a list of snakes"""
+    return sum([snake.length() for snake in snakes])/len(snakes)
