dee305853f88509da4508d0f4bdd9d032517af95
Tyson Battistella <tysonbattistella@gmail.com>
2017-03-04 10:42:57 -0800

    better name

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ def start():
         'color': '#00FF00',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
-        'name': 'sneksnek'
+        'name': '🐍 sneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksneksnek'
     }
 
 
